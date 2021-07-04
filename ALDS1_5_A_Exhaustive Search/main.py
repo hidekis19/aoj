6dac1e033d0cc3eccb5efc49965aea08f6f9acbb
DESKTOP-CJB7SVG\shira <shirahata2808@gmail.com>
2021-07-04 16:26:12 +0900

    7/4

--- ALDS1_5_A_Exhaustive Search/main.py
+++ ALDS1_5_A_Exhaustive Search/main.py
@@ -2,16 +2,24 @@ n = int(input())
 A = list(map(int,input().split()))
 q = int(input())
 M = list(map(int,input().split()))
+A = sorted(A,reverse=True)
 for m in M:
-  s = 0
-  for i in range(2 ** n):
-      x = 0
-      for j in range(n):  # このループが一番のポイント
-          if ((i >> j) & 1):  # 順に右にシフトさせ最下位bitのチェックを行う
-              x += A[j]  # フラグが立っていたら bag に果物を詰める
-      if x == m:
-        s =1
-  if s == 1:
-    print("yes")
-  else:
-    print("no")
+    s = 0
+    if sum(A) < m:
+        print('no')
+    else:
+        for i in range(2 ** n):
+            x = 0
+            for j in range(n):  # このループが一番のポイント
+                if ((i >> j) & 1):  # 順に右にシフトさせ最下位bitのチェックを行う
+                    x += A[j]  # フラグが立っていたら bag に果物を詰める
+                if x > m:
+                    break
+                elif x+sum(A[j:]) <m:
+                    break
+            if x == m:
+                s =1
+                print("yes")
+                break
+        if s==0:
+            print('no')
