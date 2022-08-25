5d2a0968ed47b0f1a268f3cf3d89852f0d871e84
hideki-shirahata <shira@hideki-shirahatanoMacBook-Air.local>
2022-08-26 01:38:47 +0900

    8/25

--- ALDS1_10_C_Longest Common Subsequence/main.py
+++ ALDS1_10_C_Longest Common Subsequence/main.py
@@ -1,53 +1,40 @@
-import sys, os, math, bisect, itertools, collections, heapq, queue, copy, array
+def dps(x,y):
+    dp = [[0 for i in range(len(y)+1)] for j in range(len(x)+1)]
 
-# from scipy.sparse.csgraph import csgraph_from_dense, floyd_warshall
-# from decimal import Decimal
-# from collections import defaultdict, deque
+    for i in range(len(x)):
+        for j in range(len(y)):
+            if x[i] == y[j]:
+                dp[i+1][j+1] = max(dp[i][j] + 1,
+                                dp[i+1][j],
+                                dp[i][j+1])
+            else:
+                if dp[i+1][j] >= dp[i][j+1]:
+                    dp[i+1][j+1] = dp[i+1][j]
+                else:
+                    dp[i+1][j+1] = dp[i][j+1]
+    return dp
 
-sys.setrecursionlimit(10000000)
+n = int(input())
+X = []
+Y = []
+for i in range(n):
+    X.append(input())
+    Y.append(input())
 
-ii = lambda: int(sys.stdin.buffer.readline().rstrip())
-il = lambda: list(map(int, sys.stdin.buffer.readline().split()))
-fl = lambda: list(map(float, sys.stdin.buffer.readline().split()))
-iln = lambda n: [int(sys.stdin.buffer.readline().rstrip()) for _ in range(n)]
+ans = []
 
-iss = lambda: sys.stdin.buffer.readline().decode().rstrip()
-sl = lambda: list(map(str, sys.stdin.buffer.readline().decode().split()))
-isn = lambda n: [sys.stdin.buffer.readline().decode().rstrip() for _ in range(n)]
+for i in range(n):
+    s = X[i]
+    t = Y[i]
+    l = dps(s,t)
+    ans.append(l[len(s)][len(t)])
 
-lcm = lambda x, y: (x * y) // math.gcd(x, y)
-
-MOD = 10 ** 9 + 7
-MAX = float('inf')
-
-
-def lcs(X, Y):
-    dp = []
-    for y in Y:
-        bgn_idx = 0
-        for i, cur_idx in enumerate(dp):
-            # 既に探索済みの最長文字列を更新
-            chr_idx = X.find(y, bgn_idx) + 1
-            if not chr_idx:
-                break
-            dp[i] = min(chr_idx, cur_idx)
-            bgn_idx = cur_idx
-        else:
-            chr_idx = X.find(y, bgn_idx) + 1
-            if chr_idx:
-                dp.append(chr_idx)
-    return len(dp)
+for a in ans:
+    print(a)
 
 
-def main():
-    if os.getenv("LOCAL"):
-        sys.stdin = open("input.txt", "r")
-
-    N = ii()
-    for _ in range(N):
-        X, Y = iss(), iss()
-        print(lcs(X, Y))
+                
 
 
-if __name__ == '__main__':
-    main()
+
+
